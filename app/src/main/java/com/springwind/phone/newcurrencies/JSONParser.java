f1c57bf49e3940328306b7f74c6b6765d595ce26
xcf <384207719@qq.com>
2017-11-30 16:23:11 +0800

    JSON parse

--- app/src/main/java/com/springwind/phone/newcurrencies/JSONParser.java
+++ app/src/main/java/com/springwind/phone/newcurrencies/JSONParser.java
@@ -2,41 +2,37 @@ package com.springwind.phone.newcurrencies;
 
 import android.net.http.HttpResponseCache;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPostHC4;
-import org.apache.http.client.methods.HttpRequestBaseHC4;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.utils.HttpClientUtils;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * Created by Muzhou on 3/16/2017.
  */
 public class JSONParser {
     static InputStream sInputStream =null;
-    static JSONParser sReturnJsonObject =null;
     static String sRawJsonString ="";
-    public JSONParser(){
+    OkHttpClient client = new OkHttpClient();
+    public JSONParser(){}
 
-    }
-    public JSONObject getJSONFromUrl(String url){
-        try {
-            CloseableHttpClient client = HttpClients.createDefault();
-            HttpPost postHC4 = new HttpPost(url);
-            HttpResponse httpResponseCache = client.execute(postHC4);
-            HttpEntity httpEntity = httpResponseCache.getEntity();
-            sInputStream = httpEntity.getContent();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public JSONObject getJSONFromUrl(String url) throws IOException, JSONException {
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        if(response.isSuccessful()){
+            return new JSONObject(response.body().string());
+        }else {
+            throw new IOException("Unexpected code"+response);
         }
-
-
     }
 }
